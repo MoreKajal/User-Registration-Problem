cea75e64a9600544c839f6b98f614723601e3eb6
Kajal-More <56627613+MoreKajal@users.noreply.github.com>
2021-02-23 07:07:19 -0800

    Update UserRegistration.java

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,7 +7,7 @@ public class UserRegistration {
     public static void validFirstName(String validFirst) {
         //String firstName = "^[A-Z]{1}[a-z]{3,}$";
         //System.out.println("Boolean Result is :" + Pattern.compile(firstName).matches(firstName, firstName));
-		System.out.println("Enter First Name: "+Pattern.matches("^[A-Z]{1}[a-z]{2,}$", validFirst));
+	System.out.println("Enter First Name: "+Pattern.matches("^[A-Z]{1}[a-z]{2,}$", validFirst));
     }
 
     public static void main(String[] args) {
