152b64f7ceeba6c7f9fa88f462a0a1df9178bc24
MoreKajal <kajalm3105@gmail.com>
2021-02-23 22:12:02 -0800

    [Kajal]Added:Code to Validate min 8 character password

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,12 +4,14 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static void validName(String validFirst, String validLast, String mobileNum, String email) {		
+    public static void validName(String validFirst, String validLast, String mobileNum, String rule1) {		
     	System.out.println("Boolean Result for First Name: "+Pattern.matches("^[A-Z]{1}[a-z]{2,}$", validFirst));
     	System.out.println("Boolean Result for Last Name :" + Pattern.matches("^[A-Z]{1}[a-z]{2,}$", validLast));
 		//System.out.println("Boolean Result for Mobile Number: " + Pattern.matches("^[+][9][1][-][7-9]{1}[0-9]{9}$", mobileNum));
 		System.out.println("Boolean Result for Mobile Number: " + Pattern.matches("^[9][1]\\s[7-9]{1}[0-9]{9}$", mobileNum));
-		System.out.println("Boolean Result for Email Id : " + Pattern.matches("^[a-zA-Z0-9.]+@[a-zA-z]$", email));
+		//System.out.println("Boolean Result for Email Id : " + Pattern.matches("^[a-zA-Z0-9.]+@[a-zA-z]$", email));
+		System.out.println("Boolean Result for Password Rule1: " + Pattern.matches("^[a-zA-Z]{8,}$", rule1));
+		
     }
 
     public static void main(String[] args) {
@@ -21,9 +23,9 @@ public class UserRegistration {
         String lastName = sc.next();
         System.out.println("Enter your Mobile Number :\n ");
         String mobNo = sc.next();
-        System.out.println("Enter your Email Id :\n ");
-        String email = sc.next();
-        validName(firstName, lastName, mobNo, email);
+        System.out.println("Enter your Password :\n ");
+        String pass1 = sc.next();
+        validName(firstName, lastName, mobNo, pass1);
     }
 }
 
