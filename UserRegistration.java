023f2e5af93f61cf8092852009e9151965867e3e
MoreKajal <kajalm3105@gmail.com>
2021-02-23 20:29:09 -0800

    [Kajal]Added:Code to Check Valid First Name

---
+++ UserRegistration.java
@@ -0,0 +1,22 @@
+package userregistration.com;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    public static void validFirstName(String validFirst) {
+        //String firstName = "^[A-Z]{1}[a-z]{3,}$";
+        //System.out.println("Boolean Result is :" + Pattern.compile(firstName).matches(firstName, firstName));
+		System.out.println("Enter First Name: "+Pattern.matches("^[A-Z]{1}[a-z]{2,}$", validFirst));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("***Welcome to User Registration Problem Solving***");
+        System.out.println("Enter your First Name :\n ");
+        final Scanner sc = new Scanner(System.in);
+        String firstName = sc.next();
+        validFirstName(firstName);
+    }
+}
+
+
